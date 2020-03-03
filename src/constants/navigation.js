64b70ad398424a46a7f894158f2657d82da9538a
blluci <uo264883@uniovi.es>
2020-03-03 12:38:11 +0100

    Added Generator App

---
+++ src/constants/navigation.js
@@ -0,0 +1,38 @@
+/**
+ * Object mapping of known possible inboxes for the user
+ */
+export const NavigationItems = [
+  {
+    id: 'welcome',
+    icon: '/img/icon/apps.svg',
+    label: 'navBar.welcome',
+    to: '/welcome'
+  },
+  {
+    id: 'profile',
+    icon: '/img/people.svg',
+    label: 'navBar.profile',
+    to: '/profile'
+  }
+];
+
+export const ProfileOptions = [
+  {
+    label: 'navBar.profile',
+    onClick: 'profileRedirect',
+    icon: 'cog'
+  },
+  {
+    label: 'navBar.formModelConvert',
+    onClick: 'formModelConvertRedirect'
+  },
+  {
+    label: 'navBar.formModelRender',
+    onClick: 'formModelRenderRedirect'
+  },
+  {
+    label: 'navBar.logOut',
+    onClick: 'logOut',
+    icon: 'lock'
+  }
+];
