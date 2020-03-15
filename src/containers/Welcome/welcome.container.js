d849116532ea4b5df83fd7b65ee40057a1c78cf4
UO263909 <UO263909@uniovi.es>
2020-03-15 22:07:30 +0100

    nice username and internationalized notif

--- src/containers/Welcome/welcome.container.js
+++ src/containers/Welcome/welcome.container.js
@@ -48,7 +48,7 @@ export class WelcomeComponent extends Component<Props> {
     const user = data[webId];
     const nameLd = await user.vcard_fn;
 
-    const name = nameLd && nameLd.value.trim().length > 0 ? nameLd.value : webId.toString();
+    const name = nameLd && nameLd.value.trim().length > 0 ? nameLd.value : webId.toString().split("//")[1].split(".")[0];
     const imageLd = await user.vcard_hasPhoto;
 
     let image;
