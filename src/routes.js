6f9c0e3bed2a3fad01b9d6f47bfc81532f4d3016
uo264074 <56980891+uo264074@users.noreply.github.com>
2020-03-17 01:10:54 +0100

    Login fixed

--- src/routes.js
+++ src/routes.js
@@ -37,7 +37,7 @@ const privateRoutes = [
 ];
 
 const Routes = () => (
-  <Router>
+  <Router basename="/viade_en1a">
     <Fragment>
       <Switch>
         <NotLoggedInLayout component={Login} path="/login" exact />
