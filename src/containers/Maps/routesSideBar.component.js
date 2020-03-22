70d3ff984dd32fc99ab8bcf236939f144bd68b01
polectron <polectron@gmail.com>
2020-03-22 13:14:19 +0100

    Add route buttons with dropdown and delete method.

--- src/containers/Maps/routesSideBar.component.js
+++ src/containers/Maps/routesSideBar.component.js
@@ -1,39 +1,15 @@
 import React, {Component} from 'react';
 import FC from 'solid-file-client';
 import auth from 'solid-auth-client';
-import { MapsSideBar } from './maps.style';
+import {MapsSideBar} from './maps.style';
 import styled from "styled-components";
-
-
-/*const RoutesHeader = () => {
-    return (<h2>Upload your routes</h2>);
-};*/
+import {MapRoute} from './components';
+import {Button} from 'react-bootstrap';
 
 const StyledRoutesSidebar = styled.div`
       height: 100vh;
       width: 20%;
     `;
-
-/**
-const Styles = {
-    text: {
-        color: 'white',
-        fontWeight: 'bold',
-        backgroundColor: 'transparent',
-        paddingLeft: 25
-      },
-      input: {
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        height: 40,
-        color: '#ffffff'
-      },
-     image: {
-        width: 40,
-        height: 40
-      }
-};
- **/
-
 
 class RoutesSideBar extends Component {
     constructor(props) {
@@ -41,10 +17,16 @@ class RoutesSideBar extends Component {
 
         this.state = {
             selectedFile: null,
-            routes: []
+            routes: [],
+            routesList: []
         };
 
-        this.fc = new FC( auth );
+        this.getPodRoutes = this.getPodRoutes.bind(this);
+        this.getPodRoutes();
+
+        this.deleteRoute = this.deleteRoute.bind(this);
+
+        this.fc = new FC(auth);
         this.uploadedFiles = false;
     }
 
@@ -52,78 +34,109 @@ class RoutesSideBar extends Component {
         this.uploadedFiles = false;
         const files = [...event.target.files];
         let routes = [...this.state.routes];
-        files.forEach((file) => {  
-            if(file.name.endsWith(".json")){
+        files.forEach((file) => {
+            if (file.name.endsWith(".json")) {
                 routes = [...routes, file];
                 this.uploadedFiles = true;
-            }else{
+            } else {
                 alert(file.name + " is not valid");
             }
-            
+
         });
         this.setState({routes});
     };
 
-    async onClickHandler(){
+    async onClickHandler() {
         var session = await auth.currentSession();
         var url = session.webId.split("profile/card#me")[0] + "routes/";
-        if(!await this.fc.itemExists(url)){
+        if (!await this.fc.itemExists(url)) {
             await this.fc.createFolder(url);
         }
-        this.state.routes.forEach( async (route) => {
+
+        this.onClearArray();
+
+        this.state.routes.forEach(async (route) => {
             //var name = this.getRouteName(route);
-            await this.fc.createFile(url + route.name , route, "text/plain");
+            await this.fc.createFile(url + route.name, route, "text/plain");
         });
         document.getElementById("btnPod").innerHTML = "Uploaded";
         document.getElementById("btnPod").disabled = true;
+
+        this.getPodRoutes();
     }
 
-    async getRouteName(route){
+    async getRouteName(route) {
         var fileReader = new FileReader();
         fileReader.readAsText(route);
-        fileReader.onload = function() {
+        fileReader.onload = function () {
             var obj = JSON.parse(fileReader.result);
             console.log(obj.routeName);
             return obj.routeName;
         }
     }
 
-    async getPodRoutes(){
+    async getPodRoutes() {
         var session = await auth.currentSession();
         var url = session.webId.split("profile/card#me")[0] + "routes/";
 
-        let folder = await this.fc.readFolder( url );
+        let folder = await this.fc.readFolder(url);
         folder.files.forEach((element) => {
-            var node = document.createElement("button"); 
-            var textnode = document.createTextNode(element.name); 
-
-            node.appendChild(textnode); 
-            document.getElementById("panel").appendChild(node);
+            console.log(element);
+            this.setState(state => ({
+                routeList: state.routesList.push({name: element.name, url: element.url})
+            }));
         });
     }
 
-    render() {
-        const RoutesData = () => {
-            const data = this.state.routes.map((route) => {
-                var routeName = this.getRouteName(route);
-                return (
-                    <li key={routeName}>{routeName}</li>
-                );
-            });
-            return <ul>{data}</ul>
+    showRoute(route) {
+        alert("Showing route "+route.name);
+    }
+
+    shareRoute(route){
+        alert(route.url);
+    }
+
+    async deleteRoute(route){
+        await this.fc.deleteFile(route.url);
+        this.onClearArray();
+        this.getPodRoutes();
+    }
+
+    listRoutes() {
+        let list = [];
+
+        for (let i = 0; i < this.state.routesList.length; i++) {
+            list.push(<MapRoute {...{
+                route: {
+                    name: this.state.routesList[i].name,
+                    url: this.state.routesList[i].url,
+                    showRoute: this.showRoute,
+                    shareRoute: this.shareRoute,
+                    deleteRoute: this.deleteRoute
+                }
+            }}/>);
         }
+
+        return list;
+    }
+
+    onClearArray = () => {
+        this.setState({ routesList: [] });
+    };
+
+    render() {
         return (
             <StyledRoutesSidebar>
                 <input type="file" name="file" accept=".json" onChange={this.onChangeHandler.bind(this)} multiple/>
-                <RoutesData/>
-                <button id="btnPod" disabled={!this.uploadedFiles}type="button" className="btn btn-success btn-block" onClick={this.onClickHandler.bind(this)}>Upload to your POD</button>
-                <button id="btnRoutes" type="button" className="btn btn-success btn-block" onClick={this.getPodRoutes.bind(this)}>Routes</button>
-                
+                <Button id="btnPod" disabled={!this.uploadedFiles} variant="primary" block
+                        onClick={this.onClickHandler.bind(this)}>Upload to your POD</Button>
+
                 <MapsSideBar>
                     Here your routes
-                    <p id="panel"></p>
+                    {this.listRoutes()}
                 </MapsSideBar>
-
+                <Button variant="primary" block
+                        onClick={this.onClearArray}>Clear</Button>
             </StyledRoutesSidebar>
         );
     }
