e95588696cc77c4fabd86ca28de7652906c614ec
Nut <nutbanderag@gmail.com>
2020-04-06 23:13:19 +0200

    More tests

---
+++ src/__tests__/notificationsChildren.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import Notifications from "../components/Notifications/children/Notifications/notifications.component";
+
+afterAll(cleanup);
+
+describe.only("Nav Bar", () => {
+  const { container } = render(
+    <Notifications
+      {...{
+        inbox: "https://example/inbox",
+        webId: "https://example/profile/card#me"
+      }}
+    />
+  );
+
+  it("renders without crashing", () => {
+    expect(container).toBeTruthy();
+  });
+});
