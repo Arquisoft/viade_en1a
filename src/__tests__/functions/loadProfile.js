dd1ad5eb86ec60c0bf77e30823c2f44d40d68da4
Adrián <UO265081@uniovi.es>
2020-04-08 20:10:43 +0200

    Share routes testing 70%

--- src/__tests__/functions/loadProfile.js
+++
@@ -1,9 +0,0 @@
-import data from '@solid/query-ldflex';
-
-export default async webId => {
-    const profile = {
-        name: data[webId].name,
-        friends: data[webId].friends
-    }
-    return profile;
-};
