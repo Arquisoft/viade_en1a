1083d47a39d23d865b77f9c7c5d8a9d08a922ccd
inadover <UO264608@uniovi.es>
2020-04-24 16:12:23 +0200

    Route Designing - Start

--- src/routes.js
+++ src/routes.js
@@ -1,67 +1,73 @@
-import React, { Fragment } from "react";
-import { PrivateLayout, PublicLayout, NotLoggedInLayout } from "@layouts";
-import { HashRouter as Router, Switch, Redirect } from "react-router-dom";
+import React, {Fragment} from "react";
+import {NotLoggedInLayout, PrivateLayout, PublicLayout} from "@layouts";
+import {HashRouter as Router, Redirect, Switch} from "react-router-dom";
 
 import {
-  Login,
-  Register,
-  PageNotFound,
-  Welcome,
-  RegistrationSuccess,
-  Profile,
-  Friends,
-  Maps,
-  Notifications,
-  ShareRoutes
+    Friends,
+    Login,
+    Maps,
+    Notifications,
+    PageNotFound,
+    Profile,
+    Register,
+    RegistrationSuccess,
+    RouteDesigner, //No lo reconoce?
+    ShareRoutes,
+    Welcome
 } from "./containers";
 
 const privateRoutes = [
-  {
-    id: "welcome",
-    path: "/welcome",
-    component: Welcome
-  },
-  {
-    id: "maps",
-    path: "/maps",
-    component: Maps
-  },
-  {
-    id: "friends",
-    path: "/friends",
-    component: Friends
-  },
-  {
-    id: "profile",
-    path: "/profile",
-    component: Profile
-  },
-  {
-    id: "notifications",
-    path: "/notifications",
-    component: Notifications
-  },
-  {
-    id: "share",
-    path: "/share",
-    component: ShareRoutes
-  }
+    {
+        id: "welcome",
+        path: "/welcome",
+        component: Welcome
+    },
+    {
+        id: "maps",
+        path: "/maps",
+        component: Maps
+    },
+    {
+        id: "friends",
+        path: "/friends",
+        component: Friends
+    },
+    {
+        id: "profile",
+        path: "/profile",
+        component: Profile
+    },
+    {
+        id: "notifications",
+        path: "/notifications",
+        component: Notifications
+    },
+    {
+        id: "share",
+        path: "/share",
+        component: ShareRoutes
+    },
+    {
+        id: "design",
+        path: "/design",
+        component: RouteDesigner
+    }
 ];
 
 const Routes = () => (
-  <Router>
-    <Fragment>
-      <Switch>
-        <NotLoggedInLayout component={Login} path="/login" exact />
-        <NotLoggedInLayout component={Register} path="/register" exact />
-        <NotLoggedInLayout path="/register/success" component={RegistrationSuccess} exact />
-        <PublicLayout path="/404" component={PageNotFound} exact />
-        <Redirect from="/" to="/welcome" exact />
-        <PrivateLayout path="/" routes={privateRoutes} />
-        <Redirect to="/404" />
-      </Switch>
-    </Fragment>
-  </Router>
+    <Router>
+        <Fragment>
+            <Switch>
+                <NotLoggedInLayout component={Login} path="/login" exact/>
+                <NotLoggedInLayout component={Register} path="/register" exact/>
+                <NotLoggedInLayout path="/register/success" component={RegistrationSuccess} exact/>
+                <PublicLayout path="/404" component={PageNotFound} exact/>
+                <Redirect from="/" to="/welcome" exact/>
+                <PrivateLayout path="/" routes={privateRoutes}/>
+                <Redirect to="/404"/>
+            </Switch>
+        </Fragment>
+    </Router>
 );
 
 export default Routes;
