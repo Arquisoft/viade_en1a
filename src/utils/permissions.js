3d002e199e3ed4afe5f072eb2fb4baa62813fbc0
uo264074 <56980891+uo264074@users.noreply.github.com>
2020-04-28 17:40:37 +0200

    Deleted unused functions

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -5,11 +5,7 @@ import { errorToaster } from '@utils';
 const checkAppPermissions = (userAppPermissions, appPermissions) =>
   appPermissions.every((permission) => userAppPermissions.includes(permission));
 
-// Function to check for a specific permission included in the app
-export const checkSpecificAppPermission = async (webId, permission) => {
-  const userAppPermissions = await AppPermission.checkPermissions(webId);
-  return userAppPermissions.permissions.includes(permission);
-};
+
 /**
  * SDK app will need all the permissions by the user pod so we check these permissions to work without any issues.
  * Error Message object is to hold the title, message, etc strings, as we can't use i18n libraries in this non-React file
@@ -37,41 +33,4 @@ export const checkPermissions = async (webId, errorMessage) => {
       href: errorMessage.href
     });
   }
-};
-
-/**
- * Helper function to fetch permissions for the game inbox, and if permissions are not set
- * correctly, then add them. This repairs a broken inbox.
- * @param inboxPath
- * @returns {Promise<void>}
- */
-export const checkOrSetInboxAppendPermissions = async (inboxPath, webId) => {
-  // Fetch app permissions for the inbox and see if Append is there
-  const inboxAcls = new AccessControlList(webId, inboxPath);
-  const permissions = await inboxAcls.getPermissions();
-  const inboxPublicPermissions = permissions.filter((perm) => perm.agents === null);
-
-  const appendPermission = inboxPublicPermissions.filter((perm) =>
-    perm.modes.includes(AccessControlList.MODES.APPEND)
-  );
-
-  if (appendPermission.length <= 0) {
-    // What do we do when the permission is missing? Add it!
-    try {
-      // Permission object to add. A null agent means Everyone
-      const permissions = [
-        {
-          agents: null,
-          modes: [AccessControlList.MODES.APPEND]
-        }
-      ];
-      const ACLFile = new AccessControlList(webId, inboxPath);
-      await ACLFile.createACL(permissions);
-    } catch (error) {
-      // TODO: Better error handling here
-      throw error;
-    }
-  }
-
-  return true;
 };
