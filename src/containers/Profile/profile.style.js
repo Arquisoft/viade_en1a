b7fe79898005bfab4e1525eb509469c7828c5f43
UO263909 <UO263909@uniovi.es>
2020-04-29 20:02:26 +0200

    refixing usability things

--- src/containers/Profile/profile.style.js
+++ src/containers/Profile/profile.style.js
@@ -5,7 +5,7 @@ export const ProfileWrapper = styled.section`
   flex: 1 0 auto;
   align-items: center;
   justify-content: center;
-  background-image: url('img/concentric-hex-pattern_2x.png');
+  background-image: url('img/fondoWelcome.png');
   background-repeat: repeat;
   padding: 60px 0;
 `;
