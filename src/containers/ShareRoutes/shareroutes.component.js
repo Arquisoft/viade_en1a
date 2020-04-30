7862e0a076bea2658ad2d6ff693ebaa693e41cef
polectron <polectron@gmail.com>
2020-05-01 01:50:19 +0200

    Share routes for groups and friends

--- src/containers/ShareRoutes/shareroutes.component.js
+++ src/containers/ShareRoutes/shareroutes.component.js
@@ -1,7 +1,17 @@
 import React from "react";
-import {Button, OverlayTrigger, Tooltip} from "react-bootstrap";
+import {Button} from "react-bootstrap";
 import {useTranslation} from "react-i18next";
 import * as Icon from "react-feather";
+import {FriendsShareContainer} from "./shareroutes.style";
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 export const ShareRoutesPageContent = (props) => {
 
@@ -17,41 +27,66 @@ export const ShareRoutesPageContent = (props) => {
     }
 
     return (
-        <div>
-            <table>
+        <FriendsShareContainer
+            className={"card"}>
+
+            <Accordion allowZeroExpanded={true}>
+
                 {
                     Object.keys(inflatedGroups).map((key) => {
-                        return (
-                            <tr>
-                                <td>
-                                    <OverlayTrigger
-                                        key={"top"}
-                                        placement={"top"}
-                                        overlay={
-                                            <Tooltip id={`tooltip-${"top"}`}>
-                                                {inflatedGroups[String(key)].reduce(
-                                                    (accumulator, currentValue) => {
-                                                        return accumulator + ((accumulator !== "") ? ", " : "") + currentValue.name;
-                                                    },
-                                                    ""
-                                                )}
-                                            </Tooltip>
-                                        }
-                                    >
-                                        <h3>{key}</h3>
-                                    </OverlayTrigger></td>
-                                <td><Button class="shareClass" id={"btn" + key} variant="primary"
-                                            onClick={shareRoute.bind(this, key)} block>
-                                    <Icon.Share2/> {t("routes.share")}
-                                </Button></td>
-                            </tr>
+                        return (<>
+
+                                <AccordionItem>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            {key}
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <table>
+                                            {
+                                                inflatedGroups[key].map(
+                                                    (friend) => {
+                                                        return (
+                                                            <tr>
+                                                                <td>{friend.name}</td>
+                                                                <td>
+                                                                    <Button class="shareClass" id={"btn" + key}
+                                                                            variant="primary"
+                                                                            onClick={share.shareRoute.bind(this, friend.webId)}
+                                                                            block>
+                                                                        <Icon.Share2/> {t("routes.share")}
+                                                                    </Button>
+                                                                </td>
+                                                            </tr>
+                                                        )
+                                                    }
+                                                )
+                                            }
+                                        </table>
+                                        <Button class="shareClass" id={"btn" + key} variant="primary"
+                                                onClick={shareRoute.bind(this, key)} block>
+                                            <Icon.Share2/> {t("routes.sharewithall")}
+                                        </Button>
+                                    </AccordionItemPanel>
+
+                                </AccordionItem>
+                            </>
                         );
                     })
                 }
-            </table>
 
-            <Button href="#/maps" variant="secondary" block><Icon.ArrowLeft/> {t("routes.return")}</Button>
-        </div>
+            </Accordion>
+
+            <Button
+                href="#/maps"
+                variant="secondary"
+                block>
+                < Icon.ArrowLeft/>
+                {t("routes.return")}
+            </Button>
+        </FriendsShareContainer>
+
     );
 
 };
