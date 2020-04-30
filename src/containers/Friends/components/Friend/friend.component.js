95347e465c0ed33484f0be2cb9100d3e9d714b6b
inadover <UO264608@uniovi.es>
2020-04-30 15:20:25 +0200

    Fixed some Travis issues

--- src/containers/Friends/components/Friend/friend.component.js
+++ src/containers/Friends/components/Friend/friend.component.js
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import {useTranslation} from "react-i18next";
 import {Dropdown, Card} from "react-bootstrap";
 import * as Icon from "react-feather";
