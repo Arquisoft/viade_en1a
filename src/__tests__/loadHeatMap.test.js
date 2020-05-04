749ea8ee195904a010a9287bfcdb0569b376b671
polectron <polectron@gmail.com>
2020-05-04 20:55:58 +0200

    Fix test loadHeatMap

--- src/__tests__/loadHeatMap.test.js
+++ src/__tests__/loadHeatMap.test.js
@@ -1,6 +1,6 @@
 import loadHeatMap from "../modules/loadHeatMap";
 
-test('test loadHeatMap.js', () => {
+test("test loadHeatMap.js", () => {
     const heatMapData = loadHeatMap();
     expect(heatMapData.length).toBe(537);
 });
