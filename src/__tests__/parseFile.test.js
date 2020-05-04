545d9cb84cd2d2a213f3ff6fdc003501fcc3516d
uo264074 <56980891+uo264074@users.noreply.github.com>
2020-05-04 16:42:11 +0200

    parseFile tests

---
+++ src/__tests__/parseFile.test.js
@@ -0,0 +1,15 @@
+import { cleanup } from "react-testing-library";
+import { getFileContent } from "../modules/parseFile";
+
+afterAll(cleanup);
+
+describe("fetFileContent tests", () => {
+  let file = new File(["test file"], test.txt);
+  let expectedOutput = "test file";
+
+  test("parses route correctly", () => {
+    return getFileContent(file, (result) => {
+      expect(result).toEqual(expectedOutput);;
+    })
+  });
+});
